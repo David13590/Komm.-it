04f2777a2605784f43c61756b9970a86f5f6aa97
David <david.flindt>
2021-12-03 11:19:08 +0100

    List Player character

--- Komm.-it-Spil/World1.java
+++ Komm.-it-Spil/World1.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-import java.util.List;
+import java.util.*;
 
 public class World1 extends World
 {
@@ -8,11 +8,20 @@ public class World1 extends World
     private int addenemy_3 = 0;
     
     public void act(){
+        //List of enemy_bullets
         List<enemy_bullet> myEnemy_BulletList = getObjects(enemy_bullet.class);
         for(enemy_bullet thisEnemyBullet : myEnemy_BulletList){
             System.out.print("*");
         }
         System.out.println("B");
+        
+        //List of Players
+        List<Player> myPlayerList = getObjects(Player.class);
+        for(Player thisPlayer : myPlayerList){
+            System.out.print("P");
+        }
+        
+        
     }
     public World1(){    
         super(800, 800, 1);
